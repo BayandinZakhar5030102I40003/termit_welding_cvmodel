2abc53a3252b8e607569dff104fd8687233202cf
BayandinZakhar5030102I40003 <bayandin.za@edu.spbstu.ru>
2026-05-13 00:37:14 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -86,11 +86,6 @@ st.markdown("""
     .stTabs [aria-selected="true"] { background: transparent !important; color: #000 !important; border-bottom: 2px solid #000; font-weight: 700; }
     img { max-height: 450px !important; object-fit: contain !important; }
 
-    
-    [data-testid="collapsedControl"] {
-        display: none !important;
-    }
-
     [data-testid="collapsedControl"] button::before {
     content: "≡" !important;
     font-size: 24px !important;
